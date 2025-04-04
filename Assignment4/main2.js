18a6e01c1e2be21b846db212005b840c1daffc52
Justin Wheeler <jjwheel336@gmail.com>
2025-04-04 03:15:50 -0400

    Event Listener implementation

--- Assignment4/main2.js
+++ Assignment4/main2.js
@@ -30,3 +30,15 @@ for(const image of images) {
         displayedImage.alt = e.target.alt;
     });
 }
+
+btn.addEventListener("click", () => { 
+    if(btn.getAttribute("class") === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+})
