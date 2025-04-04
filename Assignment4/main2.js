18566d0c9f76fcc4b4302d7646077564f26a1e39
Justin Wheeler <jjwheel336@gmail.com>
2025-04-04 03:13:43 -0400

    Adding regular and alt images

--- Assignment4/main2.js
+++ Assignment4/main2.js
@@ -5,21 +5,22 @@ Purpose: To demonstrate the fundamentals of Javascript
 File: main2.js
 */
 
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const altImages = {
+    "pic1.jpg": "Closeup of a human eye",
+    "pic2.jpg": "Closeup of a sea shell",
+    "pic3.jpg": "Closeup of some flowers",
+    "pic4.jpg": "Egyptian Hieroglyphics",
+    "pic5.jpg": "Closeup of a moth"
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
