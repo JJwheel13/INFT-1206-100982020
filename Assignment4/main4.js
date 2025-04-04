4b6643562666dbbb9496577eaa0e0dcced0ed5d4
Justin Wheeler <jjwheel336@gmail.com>
2025-04-04 18:27:58 -0400

    EvilCircle Class

--- Assignment4/main4.js
+++ Assignment4/main4.js
@@ -34,6 +34,74 @@ class Shape {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) =>{ 
+      switch(e.key) {
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+        case "d":
+          this.x += this.velX;   
+          break;   
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+    for(const ball of balls) {
+      if(ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if(distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = "Ball Count: " + count;
+        }
+      }
+    }
+  }
+}
+
 class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
